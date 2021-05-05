36a353db1e0dffd2f335cf1679d77aad9af2970e
michel-ferreira <michel_jaf580@hotmail.com>
2021-05-05 12:28:30 +0100

    API consuming

--- Cambios/Form1.Designer.cs
+++ Cambios/Form1.Designer.cs
@@ -29,13 +29,142 @@ namespace Cambios
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tb_valor = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cb_origem = new System.Windows.Forms.ComboBox();
+            this.cb_destino = new System.Windows.Forms.ComboBox();
+            this.btn_convert = new System.Windows.Forms.Button();
+            this.lbl_resultado = new System.Windows.Forms.Label();
+            this.lbl_status = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(101, 92);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Valor:";
+            // 
+            // tb_valor
+            // 
+            this.tb_valor.Location = new System.Drawing.Point(153, 92);
+            this.tb_valor.Name = "tb_valor";
+            this.tb_valor.Size = new System.Drawing.Size(290, 23);
+            this.tb_valor.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(23, 139);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(123, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Moeda de Origem:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(21, 187);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(125, 17);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Moeda de Destino:";
+            // 
+            // cb_origem
+            // 
+            this.cb_origem.FormattingEnabled = true;
+            this.cb_origem.Location = new System.Drawing.Point(153, 138);
+            this.cb_origem.Name = "cb_origem";
+            this.cb_origem.Size = new System.Drawing.Size(290, 23);
+            this.cb_origem.TabIndex = 4;
+            // 
+            // cb_destino
+            // 
+            this.cb_destino.FormattingEnabled = true;
+            this.cb_destino.Location = new System.Drawing.Point(153, 181);
+            this.cb_destino.Name = "cb_destino";
+            this.cb_destino.Size = new System.Drawing.Size(290, 23);
+            this.cb_destino.TabIndex = 5;
+            // 
+            // btn_convert
+            // 
+            this.btn_convert.Enabled = false;
+            this.btn_convert.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn_convert.Location = new System.Drawing.Point(501, 122);
+            this.btn_convert.Name = "btn_convert";
+            this.btn_convert.Size = new System.Drawing.Size(95, 49);
+            this.btn_convert.TabIndex = 6;
+            this.btn_convert.Text = "Convert";
+            this.btn_convert.UseVisualStyleBackColor = true;
+            // 
+            // lbl_resultado
+            // 
+            this.lbl_resultado.AutoSize = true;
+            this.lbl_resultado.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lbl_resultado.Location = new System.Drawing.Point(153, 256);
+            this.lbl_resultado.Name = "lbl_resultado";
+            this.lbl_resultado.Size = new System.Drawing.Size(290, 17);
+            this.lbl_resultado.TabIndex = 7;
+            this.lbl_resultado.Text = "Escolha um valor, moeda de origem e destino";
+            // 
+            // lbl_status
+            // 
+            this.lbl_status.AutoSize = true;
+            this.lbl_status.Location = new System.Drawing.Point(101, 344);
+            this.lbl_status.Name = "lbl_status";
+            this.lbl_status.Size = new System.Drawing.Size(38, 15);
+            this.lbl_status.TabIndex = 8;
+            this.lbl_status.Text = "status";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(429, 336);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(199, 23);
+            this.progressBar1.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(649, 398);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.lbl_status);
+            this.Controls.Add(this.lbl_resultado);
+            this.Controls.Add(this.btn_convert);
+            this.Controls.Add(this.cb_destino);
+            this.Controls.Add(this.cb_origem);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tb_valor);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Cambios";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tb_valor;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cb_origem;
+        private System.Windows.Forms.ComboBox cb_destino;
+        private System.Windows.Forms.Button btn_convert;
+        private System.Windows.Forms.Label lbl_resultado;
+        private System.Windows.Forms.Label lbl_status;
+        private System.Windows.Forms.ProgressBar progressBar1;
     }
 }
 
--- Cambios/Form1.cs
+++ Cambios/Form1.cs
@@ -1,21 +1,106 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿
 
 namespace Cambios
 {
+    using Cambios.Servicos;
+    using Modelos;
+    using Newtonsoft.Json;
+    using System;
+    using System.Collections.Generic;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+    using System.Windows.Forms;
+
     public partial class Form1 : Form
     {
+
+        #region Atributos
+
+        private NetworkService networkService;
+
+        private APIService apiService;
+
+        private List<Rate> Rates;
+
+        #endregion
+
         public Form1()
         {
             InitializeComponent();
+            networkService = new NetworkService();
+            apiService = new APIService();
+            LoadRates();
         }
 
+        private async void LoadRates()
+        {
+            bool load;
+
+            lbl_resultado.Text = "A atualizar taxas...";
+
+            var connection = networkService.CheckConnection();
+
+            if (!connection.IsSuccess)
+            {
+                LoadLocalRates();
+                load = false;
+                return;
+            }
+            else
+            {
+                await LoadApiRates();
+                load = true;
+            }
+
+            if(Rates.Count == 0)
+            {
+                lbl_resultado.Text = "Não há ligação à Internet" + Environment.NewLine +
+                    "e não foram previamente carregadas as taxas." + Environment.NewLine +
+                    "Por favor tente mais tarde";
+                return;
+            }
+
+
+            cb_origem.DataSource = Rates;
+            cb_origem.DisplayMember = "Name";
+
+            //corrige bug da Microsoft
+            cb_destino.BindingContext = new BindingContext();
+
+            cb_destino.DataSource = Rates;
+            cb_destino.DisplayMember = "Name";
+
+            btn_convert.Enabled = true;
+
+            lbl_resultado.Text = "Taxas atualizadas...";
+
+            if (load)
+            {
+                lbl_status.Text = string.Format("Taxas carregadas da internet em {0:F}", DateTime.Now);
+            }
+            else
+            {
+                lbl_status.Text = string.Format("Taxas carregadas da Base da Dados");
+            }
+
+            progressBar1.Value = 100;
+
+        }
+
+        private void LoadLocalRates()
+        {
+            throw new NotImplementedException();
+        }
+
+        private async Task LoadApiRates()
+        {
+            progressBar1.Value = 0;
+
+            var response = await apiService.GetRates("http://cambios.somee.com", "/api/rates");
+
+            Rates = (List<Rate>)response.Result;
+
+            progressBar1.Value = 100;
+        }
     }
 }
